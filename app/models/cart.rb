0724fb39fa16492b82df3fa487619c66e89333b6
rordev456 <devidev45@proton.me>
2016-09-01 20:08:42 +0100

    cart

---
+++ app/models/cart.rb
@@ -0,0 +1,43 @@
+class Cart
+
+  attr_reader :items
+
+  def initialize(items = [])
+    @items = items
+  end
+
+  def add_item(product_id)
+    item = @items.find { |item| item.product_id == product_id}
+    if item
+      item.increment
+    else
+      @items << CartItem.new(product_id)
+    end
+  end
+
+  def empty?
+    @items.empty?
+  end
+
+  def serialize
+    hash = @items.map do |item|
+      {
+        "product_id" => item.product_id,
+        "quantity" => item.quantity
+      }
+    end
+    {
+      "cart" => {
+        "items" => hash
+      }
+    }
+  end
+
+  def self.builds_from_hash hash
+    items = hash["cart"]["items"].map do |item_data|
+      CartItem.new(item_data["product_id"], item_data["quantity"])
+    end
+    Cart.new(items)
+  end
+
+end
---
+++ test/models/cart_test.rb
@@ -0,0 +1,46 @@
+require "test_helper"
+
+class CartTest < MiniTest::Test
+  def test_adds_one_item
+    cart= Cart.new
+    cart.add_item(1)
+    assert_equal(cart.empty?, false)
+  end
+
+  def test_adds_up_the_quantity
+    cart= Cart.new
+    3.times { cart.add_item(22) }
+    assert_equal(cart.items.length , 1)
+    assert_equal(cart.items[0].quantity , 3)
+  end
+
+  def test_retreaves_products
+    product = Product.create! name: "Tomato", price: 1
+    cart = Cart.new
+    cart.add_item(product.id)
+    assert_kind_of(Product, cart.items.first.product)
+  end
+
+  def test_serializes_to_hash
+    cart = Cart.new
+    cart.add_item(1)
+    assert_equal(cart.serialize, session_hash)
+  end
+
+  def test_builds_from_hash
+    cart = Cart.builds_from_hash(session_hash)
+    assert_equal(1, cart.items.first.product_id)
+  end
+
+  private
+
+  def session_hash
+    {
+      "cart" => {
+        "items" => [
+          {"product_id" => 1, "quantity" => 1}
+        ]
+      }
+    }
+  end
+end
